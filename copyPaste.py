6dad2af8008077c27cf6d4f553cf729669fff63d
Svetlana <gruzdevasvetlana@gmail.com>
2020-06-26 08:12:11 +0200

    feat: loop through the folder stage1
    Error handling have been changed to more efficient;
    Document addresses have been updated to standardized universal addresses;
    Files in the folder are listed and unwanted files are filtered out.

--- copyPaste.py
+++ copyPaste.py
@@ -1,34 +1,42 @@
 # CopyPaste - copying and pasting specified ranges in template
-import openpyxl as xl
+import os, openpyxl as xl
+from pathlib import Path
 
 # TODO: Loop receivedFile through content of specified folder. Folder to be selected by user.
-receivedFile = ('P:\\Documents Svetlana\\Excel training\\Marcos\\Regional templates\\' +\
-                                    'G&A_planning_template_Africa1_FCST2_2020.xlsm')
+userFolder = ('P:\\Documents Svetlana\\Excel training\\Marcos\\Regional templates\\Test\\')
+for file in os.listdir(userFolder):
+    filename = os.fsdecode(file)
+    if (filename.endswith(".xlsm") or filename.endswith(".xlsx")) and \
+                                not filename == 'G&A_planning_template_FCST2_2020.xlsm':
+        print(filename)
 
-sourceWB = xl.load_workbook(receivedFile)
-try:
-    sourceWS = sourceWB.get_sheet_by_name('Summary')
-except KeyError:
-    print('ERROR: Wrong tab name in sourse file.') # Insert name of the file which cause an error
-# TODO: To scip file with error, in the end print out list of all scipped files.
 
-# TODO: To be selected by user.
-templateFile = ('P:\\Documents Svetlana\\Excel training\\Marcos\\Regional templates\\' +\
-                                                'G&A_planning_template_FCST2_2020.xlsm')
-templateWB = xl.load_workbook(templateFile, keep_vba=True)
-templateWS = templateWB.get_sheet_by_name('Summary')
+# if not (userFolder + 'To be uploaded'):
+#     newFolder = os.makedirs(userFolder + 'To be uploaded')
+# else:
+#     newFolder = (userFolder + 'To be uploaded\\')
+# receivedFile = (userFolder + 'G&A_planning_template_Africa2_FCST2_2020.xlsm')
+# sourceWB = xl.load_workbook(receivedFile)
+# sheetnames = sourceWB.sheetnames
+# if 'Summary' in sheetnames:
+#     sourceWS = sourceWB.get_sheet_by_name('Summary')
+#     templateFile = (userFolder + 'G&A_planning_template_FCST2_2020.xlsm')
+#     templateWB = xl.load_workbook(templateFile, keep_vba=True)
+#     templateWS = templateWB.get_sheet_by_name('Summary')
 
-# Copy and pase defined columns from received file to template.
-columnList = [2, 5, 8, 9, 11, 12, 14, 15, 17, 19, 21, 23, 24, 26, 27, 28, 29, 30,
-                                            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
-for column in columnList:
-    for row in range(6, sourceWS.max_row):
-        c = sourceWS.cell(row=row, column=column)
-        templateWS.cell(row=row, column=column).value = c.value
-# Delete all unfilled rows.
-for row in range (templateWS.max_row, 6, -1):
-    if templateWS.cell(row=row, column=5).value == 'Please select':
-        templateWS.delete_rows(row)
-# TODO: Save in new folder.
-templateWB.save('P:\\Documents Svetlana\\Excel training\\Marcos\\Regional templates\\To be uploaded\\' +\
-                                    'G&A_planning_template_NEW_FCST2_2020.xlsm')
+#     # Copy and pase defined columns from received file to template.
+#     columnList = [2, 5, 8, 9, 11, 12, 14, 15, 17, 19, 21, 23, 24, 26, 27, 28, 29, 30,
+#                                                 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
+#     for column in columnList:
+#         for row in range(6, sourceWS.max_row):
+#             c = sourceWS.cell(row=row, column=column)
+#             templateWS.cell(row=row, column=column).value = c.value
+#     # Delete all unfilled rows.
+#     for row in range (templateWS.max_row, 6, -1):
+#         if templateWS.cell(row=row, column=5).value == 'Please select':
+#             templateWS.delete_rows(row)
+#     templateWB.save(newFolder + (Path(receivedFile).name))
+# else:
+#     f = open(userFolder + 'result.txt', 'w')
+#     errorFile = receivedFile.replace(userFolder, '')
+#     print('ERROR: Wrong tab name in sourse file ' + errorFile, file=f)
