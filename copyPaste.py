bbd6f10524421cf5c2323504f98d076c1b72092b
Svetlana <gruzdevasvetlana@gmail.com>
2020-07-07 13:35:53 +0200

    feat: minor changes
    Minor changes to optimize program for both English and Russian templates.

--- copyPaste.py
+++ copyPaste.py
@@ -1,13 +1,16 @@
 # CopyPaste - copying and pasting specified ranges in template
 
-import os, openpyxl as xl, win32com.client as win32
+import os, logging, openpyxl as xl, win32com.client as win32
 from pathlib import Path
 from tkinter.filedialog import askopenfilename, askdirectory
+from pprint import pprint
 
 def copyPaste(userFolder, templateFile):
+    logging.basicConfig(filename=(str(userFolder) + 'logs.txt'), level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
     f = open(userFolder + 'result.txt', 'w')
     for file in os.listdir(userFolder):
         fileName = os.fsdecode(file)
+        logging.debug(fileName)
         if (fileName.endswith(".xlsm") or fileName.endswith(".xlsx")) and not fileName == Path(templateFile).name:
             if ('To be uploaded') not in os.listdir(userFolder):
                 newFolder = os.makedirs(userFolder + 'To be uploaded')
@@ -29,19 +32,22 @@ def copyPaste(userFolder, templateFile):
                 columnList = [2, 5, 8, 9, 11, 12, 14, 15, 17, 19, 21, 23, 24, 26, 27, 28, 29, 30,
                                                             31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
                 # Identify first row to be copied from received file.
-                for row in range(1, sourceWS.max_row):
+                for row in range(1, (sourceWS.max_row + 1)):
                     if sourceWS.cell(row=row, column=1).value == 'Сценарий':
                         firstRow = (row + 1)
                 for column in columnList:
-                    for row in range(firstRow, sourceWS.max_row):
+                    for row in range(firstRow, (sourceWS.max_row + 1)):
                         c = sourceWS.cell(row=row, column=column)
                         templateWS.cell(row=row, column=column).value = c.value
+                        logging.debug(str(row) + str(column) + str(c.value))
+                        # TODO: Copy data validation
+
                 # Delete all unfilled rows.
                 for row in range (templateWS.max_row, 6, -1):
-                    if templateWS.cell(row=row, column=5).value == 'Please select':
+                    if templateWS.cell(row=row, column=5).value == 'Please select' or templateWS.cell(row=row, column=5).value == 'Выберите из списка':
                         templateWS.delete_rows(row)
                 templateWB.save(newFolder + newFileName)
-                os.remove(userFolder + fileName)
+                # os.remove(userFolder + fileName)
             else:
                 errorFile = receivedFile.replace(userFolder, '')
                 print('ERROR: Wrong tab name in sourse file ' + errorFile, file=f)
@@ -69,7 +75,7 @@ def fileCheck(newFolder, Headers):
         wb = xl.Workbooks.Open(newFile)
         ws = wb.Worksheets('Summary')
 
-        columnList = [3, 6, 10, 13, 16, 18, 20, 22]
+        columnList = [3, 6, 10, 16, 18, 20, 22] # 13 is Residency code, temporary excluded
         for column in columnList:
             for row in range(5, ws.UsedRange.Rows.Count):
                 if ws.Cells(row, column).Value == 'FORMULA':
